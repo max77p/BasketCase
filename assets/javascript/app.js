65235ac8a015b819eaa006206b67f7b89504f904
max77p <shanindrakumar@gmail.com>
2018-06-03 19:32:50 -0400

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -372,134 +372,172 @@ $("#searchButton").on("click", function myFunction(event) {
     event.preventDefault();
     //  Emptying out the container
     $('.grid').empty();
-  
+
     // Storing what is typed into the search bar
     var searchTerm = $("#searchForm").val();
     console.log(searchTerm);
-  
+
     if (event.keyCode === 13 || event.type === 'click') {
-      // Consturcting the query URL for the API
-      var queryURL = "https://api.edamam.com/search?q=" + searchTerm + "&app_id=b0ffc540&app_key=a41b1a4d8560dbb27bbf0ef650ec9bf0";
-      console.log(queryURL);
-      // API call
-      $.ajax({
-        url: queryURL,
-        method: "GET",
-      }).then(function (response) {
-        // For loop going through for 9 recipes
-        for (var i = 0; i < 10; i++) {
-          console.log(response);
-          // Getting the image from the API
-          console.log(response.hits[i].recipe.url);
-  
-  
-          // Creating the big square for the grid (main)
-          var recipeDiv = $("<div>").attr("class", "card").attr("class", "mainCard").attr("class","col-md-4");
-  
-          // Image
-          var recipeImage = $('<img src="' + response.hits[i].recipe.image + '" >').attr("class", "image").attr("data", response.hits[i].recipe.url);
-          recipeDiv.append(recipeImage);
-  
-  
-          // The card body contains the title of the recipe
-          var recipeBodyDiv = $("<div>").attr("class", "card-body");
-          recipeDiv.append(recipeBodyDiv);
-  
-          // Title of Recipe
-          var recipeTitle = $("<h5>").text(response.hits[i].recipe.label).attr("class", "title").attr("data", response.hits[i].recipe.url);
-          recipeBodyDiv.append(recipeTitle);
-  
-          // console.log(recipeDiv);
-          $(".grid").append(recipeDiv);
-  
-  
-  
-        }
-  
-  
-        // Takes you to the recipe url webpage
-  
-        $(document).on('click', '.image', function () {
-          window.open($(this).attr('data'));
-          console.log(this);
+        // Consturcting the query URL for the API
+        var queryURL = "https://api.edamam.com/search?q=" + searchTerm + "&app_id=b0ffc540&app_key=a41b1a4d8560dbb27bbf0ef650ec9bf0";
+        console.log(queryURL);
+        // API call
+        $.ajax({
+            url: queryURL,
+            method: "GET",
+        }).then(function (response) {
+            // For loop going through for 9 recipes
+            for (var i = 0; i < 10; i++) {
+                console.log(response);
+                // Getting the image from the API
+                console.log(response.hits[i].recipe.url);
+
+
+                // Creating the big square for the grid (main)
+                var recipeDiv = $("<div>").attr("class", "card").attr("class", "mainCard").attr("class", "col-md-4");
+
+                // Image
+                var recipeImage = $('<img src="' + response.hits[i].recipe.image + '" >').attr("class", "image").attr("data", response.hits[i].recipe.url);
+                recipeDiv.append(recipeImage);
+
+
+                // The card body contains the title of the recipe
+                var recipeBodyDiv = $("<div>").attr("class", "card-body");
+                recipeDiv.append(recipeBodyDiv);
+
+                // Title of Recipe
+                var recipeTitle = $("<h5>").text(response.hits[i].recipe.label).attr("class", "title").attr("data", response.hits[i].recipe.url);
+                recipeBodyDiv.append(recipeTitle);
+
+                var nutrition = response.hits[i].recipe.healthLabels;
+                for (var j = 0; j < nutrition.length; j++) {
+                    // console.log(nutrition[i]);
+                    if (nutrition[j] === "Vegetarian") {
+                        var nutritionImg = $('<img class="vegeIcon" src="assets/images/health/vegetarian.png">');
+                        recipeBodyDiv.append(nutritionImg);
+                    }
+                    else if (nutrition[j] === "Peanut-Free") {
+                        var nutritionImg = $('<img class="peanutIcon" src="assets/images/health/peanut-free.png">');
+                        recipeBodyDiv.append(nutritionImg);
+                    }
+                    else if (nutrition[j] === "Sugar-Conscious") {
+                        var nutritionImg = $('<img class="sugarIcon" src="assets/images/health/lowsugar.svg>');
+                        recipeBodyDiv.append(nutritionImg);
+                    }
+                }
+
+                // console.log(recipeDiv);
+                $(".grid").append(recipeDiv);
+
+
+
+            }
+
+
+            // Takes you to the recipe url webpage
+
+            $(document).on('click', '.image', function () {
+                window.open($(this).attr('data'));
+                console.log(this);
+            });
+
+            $(document).on('click', '.title', function () {
+                window.open($(this).attr('data'));
+                console.log(this);
+            });
+
+
         });
-  
-        $(document).on('click', '.title', function () {
-          window.open($(this).attr('data'));
-          console.log(this);
-        });
-  
-  
-      });
-  
-  
+
+
     }
-  
-  });
-  
-  
-  $(document).ready(function () {
+
+});
+
+
+$(document).ready(function () {
     //  Emptying out the container
     $('.grid').empty();
-  
+
     // Storing what is typed into the search bar
     var searchTerm = "random";
-    console.log(searchTerm);
+    // console.log(searchTerm);
     // Consturcting the query URL for the API
     var queryURL = "https://api.edamam.com/search?q=" + searchTerm + "&app_id=b0ffc540&app_key=a41b1a4d8560dbb27bbf0ef650ec9bf0";
-    console.log(queryURL);
+    // console.log(queryURL);
     // API call
     $.ajax({
-      url: queryURL,
-      method: "GET",
+        url: queryURL,
+        method: "GET",
     }).then(function (response) {
-      // For loop going through for 9 recipes
-      for (var i = 0; i < 10; i++) {
-        console.log(response);
-        // Getting the image from the API
-        console.log(response.hits[i].recipe.url);
-  
-  
-        // Creating the big square for the grid (main)
-        var recipeDiv = $("<div>").attr("class", "card").attr("class", "mainCard").attr("class","col-md-4");
-  
-        // Image
-        var recipeImage = $('<img src="' + response.hits[i].recipe.image + '" >').attr("class", "image").attr("data", response.hits[i].recipe.url);
-        recipeDiv.append(recipeImage);
-  
-  
-        // The card body contains the title of the recipe
-        var recipeBodyDiv = $("<div>").attr("class", "card-body");
-        recipeDiv.append(recipeBodyDiv);
-  
-        // Title of Recipe
-        var recipeTitle = $("<h5>").text(response.hits[i].recipe.label).attr("class", "title").attr("data", response.hits[i].recipe.url);;
-        recipeBodyDiv.append(recipeTitle);
-  
-        // console.log(recipeDiv);
-        $(".grid").append(recipeDiv);
-  
-  
-  
-      }
-  
-      // Takes you to the recipe url webpage
-  
-      $(document).on('click', '.image', function () {
-        window.open($(this).attr('data'));
-        console.log(this);
-      });
-  
-      $(document).on('click', '.title', function () {
-        window.open($(this).attr('data'));
-        console.log(this);
-      })
-  
-  
+        // For loop going through for 9 recipes
+        for (var i = 0; i < 10; i++) {
+            console.log(response);
+            // // Getting the image from the API
+            console.log(response.hits[i].recipe.url);
+
+
+            // Creating the big square for the grid (main)
+            var recipeDiv = $("<div>").attr("class", "card").attr("class", "mainCard").attr("class", "col-md-4");
+
+            // Image
+            var recipeImage = $('<img src="' + response.hits[i].recipe.image + '" >').attr("class", "image").attr("data", response.hits[i].recipe.url);
+            recipeDiv.append(recipeImage);
+
+
+            // The card body contains the title of the recipe
+            var recipeBodyDiv = $("<div>").attr("class", "card-body");
+            recipeDiv.append(recipeBodyDiv);
+
+            // Title of Recipe
+            var recipeTitle = $("<h5>").text(response.hits[i].recipe.label).attr("class", "title").attr("data", response.hits[i].recipe.url);;
+            recipeBodyDiv.append(recipeTitle);
+
+            //add nutrition info here
+            var nutrition = response.hits[i].recipe.healthLabels;
+            for (var j = 0; j < nutrition.length; j++) {
+                // console.log(nutrition[i]);
+                if (nutrition[j] === "Vegetarian") {
+                    var nutritionImg = $('<img class="vegeIcon" src="assets/images/health/vegetarian.png">');
+                    recipeBodyDiv.append(nutritionImg);
+                }
+                else if (nutrition[j] === "Peanut-Free") {
+                    var nutritionImg = $('<img class="peanutIcon" src="assets/images/health/peanut-free.png">');
+                    recipeBodyDiv.append(nutritionImg);
+                }
+                else if (nutrition[j] === "Sugar-Conscious") {
+                    var nutritionImg = $('<img class="sugarIcon" src="assets/images/health/lowsugar.png">');
+                    recipeBodyDiv.append(nutritionImg);
+                }
+            }
+
+            // console.log(recipeDiv);
+            $(".grid").append(recipeDiv);
+
+
+
+
+
+        }
+
+        // Takes you to the recipe url webpage
+
+        $(document).on('click', '.image', function () {
+            window.open($(this).attr('data'));
+            console.log(this);
+        });
+
+        $(document).on('click', '.title', function () {
+            window.open($(this).attr('data'));
+            console.log(this);
+        })
+
+
     });
-  
-  })
-  
+
+})
+
+
 
 
 
